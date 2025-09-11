55afedf5f163784cc28c2009aeb08685c34d69e9
Uttam Sharma <74849594+uttxms@users.noreply.github.com>
2025-09-10 23:53:02 -0700

    Update GitHub and LinkedIn links in contactLinks

--- src/contactLinks.tsx
+++ src/contactLinks.tsx
@@ -1,6 +1,6 @@
 const contactLinks = {
-  github: "https://github.com/xbftw",
-  linkedin: "https://www.linkedin.com/in/uttamssharma",
+  github: "https://github.com/uttxms",
+  linkedin: "https://www.linkedin.com/in/uttxms",
   email: "mailto:contact@uttamsharma.com",
   discord: "https://discord.com/users/415687999641354250",
   instagram: "https://instagram.com/uttxms",
